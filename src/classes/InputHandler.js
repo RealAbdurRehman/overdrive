f77c19ffb71d313f84e7f1ca59fa3a9655fa8730
Abdur Rehman <claudethebitplayer@gmail.com>
2025-01-31 17:57:48 +0500

    Update InputHandler.js

--- src/classes/InputHandler.js
+++ src/classes/InputHandler.js
@@ -11,7 +11,10 @@ export default class InputHandler {
           code === "KeyS" ||
           code === "KeyA" ||
           code === "KeyD" ||
-          code === "Space") &&
+          code === "ArrowUp" ||
+          code === "ArrowDown" ||
+          code === "ArrowLeft" ||
+          code === "ArrowRight") &&
         !this.keys.includes(code)
       )
         this.keys.push(code);
